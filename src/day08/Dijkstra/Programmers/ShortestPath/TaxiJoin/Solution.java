df0d450b37e52cb1606514a752d0fab9a830dee6
Tessssssssy <ewoo97@naver.com>
2023-12-14 14:11:42 +0900

    [File] 231214 Kakao-TaxiJoin solution.java 추가

--- src/day08/Dijkstra/Programmers/ShortestPath/TaxiJoin/Solution.java
+++ src/day08/Dijkstra/Programmers/ShortestPath/TaxiJoin/Solution.java
@@ -8,58 +8,45 @@ import java.util.Arrays;
  */
 
 public class Solution {
-    static final int INF = 10000;
+    static final int INF = 200*100000+1;
     boolean[] visited;
     int[] costs;
     int[] path;
 
-    public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = 0;
-
-        int[] result = findPath(n, s, a, b, fares);
-        int aCost = result[a-1];
-        int bCost = result[b-1];
-
-        System.out.println("A까지의 비용: " + aCost);
-        System.out.println("B까지의 배용: " + bCost);
-
-        for (int res : result) {
-            System.out.print(res + " ");
-        }
-        return answer;
-    }
-
-    public int[] findPath(int n, int s, int a, int b, int[][] fares) {
-        visited = new boolean[n];
-        costs = new int[n];
-        path = new int[n];
+    public int[] findPath(int N, int s, int[][] graph) {
+        visited = new boolean[N];
+        costs = new int[N];
+        path = new int[N];
 
         Arrays.fill(costs, INF);
 
-        int startNode = s - 1;
+        int startNode = s-1;
         costs[startNode] = 0;
 
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < N - 1; i++) {
             int minNode = findMinNode(costs, visited);
 
             visited[minNode] = true;
 
-            for (int[] edge : fares) {
-                int from = 0;
-                int to = 0;
-                int cost = 0;
+            for (int[] edge : graph) {
+                int from;
+                int to;
+                int cost;
+                from = 0;
+                to=0;
+                cost=0;
 
-                if (minNode == edge[0] - 1) {
-                    from = edge[0] - 1;
-                    to = edge[1] - 1;
+                if(minNode == edge[0]-1) {
+                    from = edge[0]-1;
+                    to = edge[1]-1;
                     cost = edge[2];
-                } else if (minNode == edge[1] - 1) {
-                    from = edge[1] - 1;
-                    to = edge[0] - 1;
+                } else if(minNode == edge[1]-1) {
+                    from = edge[1]-1;
+                    to = edge[0]-1;
                     cost = edge[2];
                 }
 
-                if (!visited[to] && from == minNode && costs[from] + cost < costs[to]) {
+                if (!visited[to] && from == minNode && costs[from] != INF && costs[from] + cost < costs[to]) {
                     costs[to] = costs[from] + cost;
                     path[to] = from;
                 }
@@ -67,17 +54,33 @@ public class Solution {
         }
         return costs;
     }
-
     private int findMinNode(int[] costs, boolean[] visited) {
         int minCost = INF;
-        int minNode = -1;
+        int minNode = 0;
 
         for (int i = 0; i < costs.length; i++) {
             if (!visited[i] && costs[i] < minCost) {
-                minCost = costs[i];
+                minCost = (int) costs[i];
                 minNode = i;
             }
         }
+
         return minNode;
     }
+
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = INF;
+
+        int[] startA = findPath(n, a, fares);
+        int[] startB = findPath(n, b, fares);
+        int[] start = findPath(n, s, fares);
+
+        for(int i=1;i<n;i++) {
+
+            answer = Math.min(answer, startA[i]+startB[i]+start[i]);
+        }
+
+        return answer;
+    }
 }
+
--- src/day08/Dijkstra/Programmers/ShortestPath/TaxiJoin/SolutionMain.java
+++ src/day08/Dijkstra/Programmers/ShortestPath/TaxiJoin/SolutionMain.java
@@ -14,6 +14,8 @@ public class SolutionMain {
         int[][] fares = {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}};
 
         Solution solution = new Solution();
-        solution.solution(n, s, a, b, fares);
+        int answer = solution.solution(n, s, a, b, fares);
+        System.out.println(answer);
+        // 82
     }
 }
